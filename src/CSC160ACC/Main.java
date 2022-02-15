aecdca4faa14fbfa036026e577e7097b8367eb64
Afr0-Samurai <98551397+Afr0-Samurai@users.noreply.github.com>
2022-02-14 20:46:49 -0700

    Initial commit

---
+++ src/CSC160ACC/Main.java
@@ -0,0 +1,99 @@
+package CSC160ACC;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
+365
+        366
+        367
+        368
+        369
+        370
+        371
+        372
+        373
+        374
+        375
+        376
+        377
+        378
+        379
+        380
+        381
+        382
+        383
+        384
+        385
+        386
+        387
+        388
+        389
+        390
+        391
+        392
+        393
+        394
+        395
+        396
+        397
+        398
+        399
+        400
+        401
+        402
+        403
+        404
+        405
+        406
+        407
+        408
+        ⌄
+        ⌄
+        import java.util.Scanner;  // Import the Scanner class
+/*    Format Specifier    Output                                      Example
+    %b                  A Boolean value                             True or false
+    %c                  A character                                 ‘a’
+    %d                  A decimal integer                           200
+    %f                  A floating-point number                     45.460000
+    %e                  A number in standard scientific notation    4.556000e+01
+    %s                  A string                                    “Java is cool”
+*/
+        ​
+        ​
+        ​
+        /*      Example           Output
+              %5c               Output the character and add four spaces before the character item, because the width is 5.
+              %6b               Output the Boolean value and add one space before the false value and two spaces before the true value.
+              %5d               Output the integer item with width 5. If the number of digits in the item is < 5, add spaces before the number. If the number of digits in the item is > 5 the width is automatically increased.
+              ​
+              %10.2f            Output the floating-point item with width 10 including a decimal point and two digits after the point. Thus, there are seven digits allocated before the decimal point. If the number of digits before the decimal point in the item is < 7 add spaces before the number. If the number of digits before the decimal point in the item is > 7 the width is automatically increased.
+              ​
+              %10.2e            Output the floating-point item with width 10 including a decimal point, two digits after the point and the exponent part. If the displayed number in scientific notation has width < 10 add spaces before the number.
+              ​
+              %12s              Output the string with width 12 characters. If the string item has fewer than 12 characters, add spaces before the string. If the string item has more than 12 characters, the width is automatically increased.
+        */
+        ​
+        ​
+        // If an item requires more spaces than the specified width, the width is automatically increased.
+//      System.out.printf("%3d#%2s#%4.2f\n", 1234, "Java", 51.6653);
+        ​
+        ​
+        //      System.out.printf("%3d#%2s#%4.2f\n", 12, "J", 51.6653);
+        ​
+        ​
+        ​
+        ​
+        ​
+        ​
+        ​
+        ​
+        ​
+        ​
+        ​
+        ​
+        ​
+        }
+        }
